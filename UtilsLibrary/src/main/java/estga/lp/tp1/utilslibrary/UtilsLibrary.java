65d63c6cd9e13006026d9d17f082556172e33c36
crist <crist@192.168.49.69>
2025-03-10 17:19:30 +0000

    teste

---
+++ UtilsLibrary/src/main/java/estga/lp/tp1/utilslibrary/UtilsLibrary.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package estga.lp.tp1.utilslibrary;
+
+/**
+ *
+ * @author crist
+ */
+public class UtilsLibrary {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
