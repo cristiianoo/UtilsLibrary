f25769450a185c4e4eef640dcd9c7c44216ee4ca
DudsTheBoy <duarteamaral@ua.pt>
2025-03-21 10:19:02 +0000

    Duarte

--- UtilsLibrary/src/main/java/estga/lp/tp1/utilslibrary/ConversaoDistancia.java
+++ UtilsLibrary/src/main/java/estga/lp/tp1/utilslibrary/ConversaoDistancia.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package estga.lp.tp1.utilslibrary;
+package utils;
 
 public class ConversaoDistancia {
     public static double converterDistancia(double valor, String unidadeOrigem, String unidadeDestino) {
