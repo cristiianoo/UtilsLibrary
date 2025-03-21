184308977612b7fcf8d439858b1de4d640c0d116
cristiianoo <90054473+cristiianoo@users.noreply.github.com>
2025-03-21 10:16:59 +0000

    Cristiano

--- UtilsLibrary/src/main/java/estga/lp/tp1/utilslibrary/FifoBuffer.java
+++ UtilsLibrary/src/main/java/estga/lp/tp1/utilslibrary/FifoBuffer.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package estga.lp.tp1.utilslibrary;
+package utils;
 
 import java.util.LinkedList;
 import java.util.Queue;
